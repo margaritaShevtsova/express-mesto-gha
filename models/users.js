9ab2653cb65ab85b60c07432b8d9f1f5e17227a8
Margarita Shevtsova <molegovn@mail.ru>
2023-06-15 18:04:13 +0300

    ПР 13

--- models/users.js
+++ models/users.js
@@ -4,19 +4,19 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, 'Поле "name" должно быть заполнено'],
-      minlength: [2, 'Минимальная длина поля "name" - 2'],
-      axlength: [30, 'Максимальная длина поля "name" - 30'],
+      required: true,
+      minlength: 2,
+      axlength: 30,
     },
     about: {
       type: String,
-      required: [true, 'Поле "name" должно быть заполнено'],
-      minlength: [2, 'Минимальная длина поля "name" - 2'],
-      maxlength: [30, 'Максимальная длина поля "name" - 30'],
+      required: true,
+      minlength: 2,
+      maxlength: 30,
     },
     avatar: {
       type: String,
-      required: [true, 'Поле "name" должно быть заполнено'],
+      required: true,
     },
   },
   { versionKey: false },
