2074fbbbedde092562653094425df2977fa066ed
Margarita Shevtsova <molegovn@mail.ru>
2023-06-28 22:01:54 +0300

    PR 14

--- routes/cards.js
+++ routes/cards.js
@@ -9,7 +9,7 @@ router.get('/cards', getCards);
 router.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(/^https?:\/\/(www\.)?[-a-zA-Z0-9-._~:/?#@!$&'()*+,;=/]{1,256}#?/),
   }),
 }), createCard);
 router.delete('/cards/:cardId', celebrate({
