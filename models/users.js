b144050ce3477200e996ca800a33d11ed857328f
Margarita Shevtsova <molegovn@mail.ru>
2023-06-28 18:06:50 +0300

    ПР 14

--- models/users.js
+++ models/users.js
@@ -33,7 +33,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
       validate: (value) => {
-        const urlRegex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
+        const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
         return urlRegex.test(value);
       },
     },
